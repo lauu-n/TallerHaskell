8a0c06251bbd7a1fe64c47827a634ffd42bb03ea
lau <laura.nino.rosas@gmail.com>
2025-08-20 08:47:34 -0500

    Problema 3

--- Problema3.hs
+++ Problema3.hs
@@ -1,12 +1,13 @@
-parImpar :: Int -> String
-parImpar n =
-    if even n
-        then "par"
-        else "impar"
+division :: (Eq a, Fractional a, Show a) => a -> a -> String
+division _ 0 = "Error: división por cero"
+division x y = "Resultado: " ++ show (x / y)
 
 main :: IO ()
 main = do
-    putStrLn "Ingrese un número:"
-    input <- getLine
-    let numero = read input :: Int
-    putStrLn $ "El número es " ++ parImpar numero
+    putStrLn "Ingrese el primer número::"
+    numInput <- getLine
+    putStrLn "Ingrese el segundo número:"
+    denInput <- getLine
+    let numerador = read numInput :: Double
+    let denominador = read denInput :: Double
+    putStrLn $ division numerador denominador
